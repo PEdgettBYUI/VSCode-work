6ca477986050ac670782c99cc91f7fcadacf91ad
PEdgettBYUI <edg24004@byui.edu>
2024-10-07 11:46:20 -0600

    Changed round( , 2) to .2f

--- cse110_programs/week_3/meal_price_calculator.py
+++ cse110_programs/week_3/meal_price_calculator.py
@@ -18,13 +18,22 @@ st_value = meal_subtotal * sales_tax_rate/100
 total = meal_subtotal + st_value
 
 # Results
-print(f"\nSubtotal: ${round(meal_subtotal, 2)}")
-print(f"Sales Tax: ${round(st_value, 2)}")
-print(f"Total: ${round(total, 2)}\n")
+# # Original rounding using the round() function
+# print(f"\nSubtotal: ${round(meal_subtotal, 2)}")
+# print(f"Sales Tax: ${round(st_value, 2)}")
+# print(f"Total: ${round(total, 2)}\n")
+
+# Rounded using :.#f
+# : indicates that you are about to format it
+# . indicates precision rounded to
+# f indicates "fixed point" notation; note: e is used for exponent notation
+print(f"\nSubtotal: ${meal_subtotal:.2f}")
+print(f"Sales Tax: ${st_value:.2f}")
+print(f"Total: ${total:.2f}\n")
 
 # Estimating change
 payment = float(input("What is the payment amount? $"))
-print(f"Change: ${round(payment - total, 2)}")
+print(f"Change: ${payment - total:.2f}")
 # Closing Message
 print("\n----------------------------")
 print("Thank you for using the Mealizer900, \nwe hope you have a great day, and enjoy your meal!\n")
