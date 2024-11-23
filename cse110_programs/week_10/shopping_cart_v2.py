0c28a49eee42e744ff4dbefa0c0ed84040bb51dc
ContraZombie4 <gameking599@gmail.com>
2024-11-23 15:07:04 -0700

    finished shopping_cart_v2.py;

--- cse110_programs/week_10/shopping_cart_v2.py
+++ cse110_programs/week_10/shopping_cart_v2.py
@@ -9,8 +9,10 @@ menu_top = ["1. Add Item", "2. View Cart", "3. Remove Item", "4. Compute Total",
 
 selection = 0
 cart = []
+prices = []
 item = ""
 price = 0.00
+total = 0.00
 
 while selection != 5:
     print("Please select one of the following: ")
@@ -21,17 +23,31 @@ while selection != 5:
     if selection == 1:
         item = input("What item would you like to add? ")
         cart.append(item)
-        # price = input(f"What is the price of '{item}'? ")
-        # print(f"'{item}' has been added to the cart.\n")
+
+        price = float(input(f"What is the price of '{item}'? "))
+        print(f"'{item}' has been added to the cart.\n")
+        prices.append(price)
         print()
     elif selection == 2:
-        for each in cart:
-            print(f" - {each}")
+        for i, each in enumerate(cart):
+            print(f" {i + 1}. {each} ${prices[i]:.2f}")
         time.sleep(2)
         print()
     elif selection == 3:
-        print("Removing Items to be added in a later update")
+        choice = int(input("Which item would you like to remove? "))
+        remove_cart = cart.pop(choice-1)
+        remove_price = prices.pop(choice-1)
+        print(f"Item \"{remove_cart} - ${remove_price}\" removed.")
+        time.sleep(2)
+        print()
     elif selection == 4:
-        print("Computing total to be added in a later update")
+        total = 0.00    # Clears total to sum updated lists
+        for i, item in enumerate(cart):
+            total += prices[i]
+        print(f"Your Running Total is: ${total:.2f}")
+        time.sleep(2)
+        print()
+
 
 print("Shopping concluded. Thank you. Goodbye.")
+print()
